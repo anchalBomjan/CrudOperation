f0c0fc5ce8adf6ba94063012eba431b758aa21a0
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-06-24 15:01:42 +0545

    Add project files.

---
+++ CrudOperation/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using CrudOperation.Models;
+
+namespace CrudOperation.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> Get();
+        Task<ProductModel> Find(Guid uid);
+        Task<ProductModel> Add(ProductModel model);
+        Task<ProductModel> Update(ProductModel model);
+        Task<ProductModel> Remove(ProductModel model);
+    }
+}
